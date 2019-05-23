de6761115007c6cbe47661c4d14f316eae7b037e
Liu Hanyi <161250072@smail.nju.edu.cn>
2019-05-24 03:30:30 +0800

    commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/admin")
+@SpringBootApplication
+public class DemoApplication {
+    @RequestMapping("/home")
+    String home(HttpServletRequest request) {
+        String userName = "Jef";
+        int count = 101;
+        return "欢迎您" + userName + "，这是您的第 " + count + " 次登录";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
